df2d58a112061d4475a5cd281ae518a11c2b18d2
Kinga <niu.ula.a@poczta.fm>
2020-05-16 14:18:11 +0200

    Create Event Class

---
+++ src/main/java/com/helpit/Events/Event.java
@@ -0,0 +1,65 @@
+package com.helpit.Events;
+
+import javax.persistence.*;
+
+
+@Entity
+public class Event {
+
+    private Long id;
+    private String date;
+    private String time;
+    private String name;
+    private String foundation;
+    private String description;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFoundation() {
+        return foundation;
+    }
+
+    public void setFoundation(String foundation) {
+        this.foundation = foundation;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
