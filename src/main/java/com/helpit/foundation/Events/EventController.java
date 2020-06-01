8a72d4eb71c01e81d5339fe299297c208e737a80
Kinga <niu.ula.a@poczta.fm>
2020-06-01 14:39:52 +0200

    Delete Event

--- src/main/java/com/helpit/foundation/Events/EventController.java
+++ src/main/java/com/helpit/foundation/Events/EventController.java
@@ -16,28 +16,29 @@ public class EventController {
     private EventRepository repo;
 
     @GetMapping("/events")
-    public String view(Model model)
-    {
+    public String view(Model model) {
         List<Event> listEvents = listAll();
         model.addAttribute("listEvents", listEvents);
-
         return "events/index";
     }
 
     @GetMapping("/add")
-    public String add(Model model)
-    {
+    public String add(Model model) {
         Event event = new Event();
         model.addAttribute("event", event);
-
         return "events/add";
     }
 
     @RequestMapping(value="/create_event", method = RequestMethod.POST)
-    public String create_event_submit(@Valid @ModelAttribute("event") Event event)
-    {
+    public String create_event_submit(@Valid @ModelAttribute("event") Event event) {
         save(event);
         return "events/show";
+    }
+
+    @RequestMapping("/delete/{id}")
+    public String delete_event(@Valid @ModelAttribute("event") Event event) {
+        delete(event.getId());
+        return "events/del";
     }
 
     public List<Event> listAll() {
--- target/classes/schemaevents.sql
+++ target/classes/schemaevents.sql
@@ -5,3 +5,19 @@ create table if not exists events (
     foundation varchar(30),
     description varchar(200)
 );
+
+create table if not exists foundation (
+    id BIGINT AUTO_INCREMENT,
+    foundation_name varchar(50),
+    foundation_owner_name varchar(50),
+    foundation_owner_surname varchar(50)
+);
+
+ADD INDEX `foundation_idx` (`foundation` ASC) VISIBLE;
+;
+ALTER TABLE `helpit`.`event`
+    ADD CONSTRAINT `foundation`
+        FOREIGN KEY (`foundation`)
+            REFERENCES `helpit`.`foundation` (`id`)
+            ON DELETE NO ACTION
+            ON UPDATE NO ACTION;
