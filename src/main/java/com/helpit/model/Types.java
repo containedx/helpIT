e8f1945ce1db759a0c66e757dc09c2f1c9e3031d
Adrian <adrianj66@interia.pl>
2020-07-07 13:17:25 +0200

    add and test new features: delete articles by user, filer articles by category and foundation on volunteer view

--- src/main/java/com/helpit/model/Types.java
+++ src/main/java/com/helpit/model/Types.java
@@ -20,4 +20,6 @@ public enum Types {
     public String getTypeName () {
         return typeName;
     }
+
+
 }
--- src/main/java/com/helpit/web/WebConfig.java
+++ src/main/java/com/helpit/web/WebConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.context.request.WebRequest;
 
 import java.util.HashSet;
@@ -15,9 +16,6 @@ import java.util.Set;
 @Configuration
 @Controller
 public class WebConfig {
-
-
-
 
     @GetMapping("/signup")
     public String showRegistrationPage(){
@@ -48,16 +46,23 @@ public class WebConfig {
         return "registration/volunteer";
     }
 
+    @RequestMapping({"/user/manager"})
+    public String getUserManager()
+    {
+        return "manager";
+    }
 
 
+    @RequestMapping({"/foundation/edit"})
+    public String getFoundationEdit()
+    {
+        return "/foundation/edit";
+    }
 
     @Bean
     public BCryptPasswordEncoder passwordEncoder(){
         BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
         return bCryptPasswordEncoder;
     }
-
-
-
 
 }
